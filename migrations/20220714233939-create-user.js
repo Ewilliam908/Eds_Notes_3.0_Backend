7c3d0eed2822e74224e234505c32aecdbfa3afa6
Edwin William <edwinwilliam908@gmail.com>
2022-07-16 15:49:42 -0400

    tasks migrations

--- migrations/20220714233939-create-user.js
+++ migrations/20220714233939-create-user.js
@@ -2,20 +2,25 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
+    await queryInterface.createTable('users', {
+      users_id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    }
+ })
+},
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('users')
   }
 };
--- migrations/20220716035217-create_tasks.js
+++ migrations/20220716035217-create_tasks.js
@@ -2,20 +2,21 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
+    await queryInterface.createTable('tasks', {
+      tasks_id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    description: {
+      type: Sequelize.TEXT,
+      allowNull: false,
+    }
+ })
+},
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('tasks')
   }
 };
